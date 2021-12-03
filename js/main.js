5ffb01d0384345031403de521f608039c745b4f5
Maksim Chekhovskiy <veles.cafe@gmail.com>
2021-12-03 11:52:02 +0200

    add main block

--- js/main.js
+++ js/main.js
@@ -9,4 +9,27 @@ $(function () {
         
     });
 
+    $('.product__name ').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            focusOnSelect: true,
+            vertical: true,
+//   arrows: false,
+//   fade: true,
+        asNavFor: '.prodact__content ',
+        prevArrow:   '<button type="button" class="up-crab"> <img src="./images/crab/up-crab.png" alt=""> </button>',
+        nextArrow: '<button type="button" class="down-crab"> <img src="./images/crab/down-crab.png" alt=""> </button>',
+        centerMode: true,
+});
+    $('.prodact__content ').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            asNavFor: '.product__name',
+            fade: true,
+            arrows: false
+//   dots: true,
+            
+
+});
+
 });
