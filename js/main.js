7b86d2d73e80c4dc85a0371a775a593dad9fde88
Maksim Chekhovskiy <veles.cafe@gmail.com>
2021-12-07 22:08:21 +0200

    m

--- js/main.js
+++ js/main.js
@@ -5,7 +5,17 @@ $(function () {
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"> <img src="./images/svg/up.svg" alt=""> </button>',
         nextArrow: '<button type="button" class="slick-next"> <img src="./images/svg/down.png" alt=""> </button>',
-        fade: true
+        fade: true,
+        responsive: [{
+            breakpoint: 361,
+            settings:{
+                dots: false,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000
+            }
+        }
+        ]
         
     });
 
@@ -14,12 +24,25 @@ $(function () {
             slidesToScroll: 1,
             focusOnSelect: true,
             vertical: true,
+            
 //   arrows: false,
 //   fade: true,
         asNavFor: '.prodact__content ',
         prevArrow:   '<button type="button" class="up-crab"> <img src="./images/crab/up-crab.png" alt=""> </button>',
         nextArrow: '<button type="button" class="down-crab"> <img src="./images/crab/down-crab.png" alt=""> </button>',
         centerMode: true,
+        responsive: [{
+            breakpoint: 891,
+            settings:{
+                vertical: false,
+                slidesToShow: 1,
+                arrows: false,
+                centerMode: true,
+                dots: true,
+            }
+        }
+        ]
+        
 });
     $('.prodact__content ').slick({
             slidesToShow: 1,
